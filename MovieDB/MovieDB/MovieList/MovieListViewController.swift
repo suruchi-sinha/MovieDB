992c25ad448ba6b4e56186d024fc5d97eea2bbb9
Suruchi Sinha <suruchi.sinha@deliveroo.co.uk>
2023-03-06 21:10:39 +0000

    Added groups and group files

---
+++ MovieDB/MovieDB/MovieList/MovieListViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class MovieListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
